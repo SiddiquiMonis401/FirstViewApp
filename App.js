388bee2fe9d6fa4688a60cde0df2378cb464c447
SiddiquiMonis401 <monisms15@gmail.com>
2020-06-09 04:06:40 +0500

    Added properties values from the dynamic data with v-bind header :)

--- App.js
+++ App.js
@@ -2,5 +2,12 @@ new Vue({
     el:"#app",
     data:{
         name:"Monis Mazhar",
+        job:'Web Developer',
+        website:'https://github.com/SiddiquiMonis401/FirstViewApp'
+    },
+    methods:{
+        greet: function(time){
+         return "Greetings to " + " " +  this.name + " "+ time;
+        }
     }
 })
