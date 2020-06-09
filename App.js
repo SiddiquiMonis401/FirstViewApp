b13ff61df5beb4beef97032dec8cbf11224c9a09
SiddiquiMonis401 <monisms15@gmail.com>
2020-06-10 01:12:55 +0500

    Learn The v-if and v-show directives and also about the difference that v-show sets the display property of the element to none while v-if remove or add the element to the DOM :( :) :)_(:

--- App.js
+++ App.js
@@ -1,24 +1,13 @@
 new Vue({
   el: '#app',
   data: {
-      isRed:false,
-      isBlue:true,
+      isError: false,
+      isSuccess: false,
   },
   methods: {
-   setRed: function () {
-       this.isRed = !this.isRed;
-       this.isBlue = !this.isBlue;
-   }
 
   },
   computed:{
-      addToA: function () {
-          console.log("Add to A")
-          return this.age + this.a;
-      },
-      addToB: function () {
-          console.log("Add to B")
-          return this.age + this.b;
-      }
+
   }
 })
