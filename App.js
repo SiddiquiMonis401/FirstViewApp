d3d61e578ccf99d14b01a43dad6d00a8235711e3
SiddiquiMonis401 <monisms15@gmail.com>
2020-06-09 22:20:14 +0500

    Events and events modifier

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 new Vue({
-    el:"#app",
-    data:{
-        name:"Monis Mazhar",
-        job:'Web Developer',
-        website:'https://github.com/SiddiquiMonis401/FirstViewApp'
+  el: '#app',
+  data: {
+    name: 'Monis Mazhar',
+    job: 'Web Developer',
+    website: 'https://github.com/SiddiquiMonis401/FirstViewApp',
+    age: 25,
+    xOffset: 0,
+    yOffset: 0
+  },
+  methods: {
+    greet: function (time) {
+      return 'Greetings to ' + ' ' + this.name + ' ' + time
     },
-    methods:{
-        greet: function(time){
-         return "Greetings to " + " " +  this.name + " "+ time;
-        }
+
+    add: function (inc) {
+      this.age += inc
+    },
+
+    subtract: function (dec) {
+      this.age += dec
+    },
+
+    setXY: function (event){
+        this.xOffset = event.offsetX;
+        this.yOffset = event.offsetY;
     }
+  }
 })
