9a30a41e40e6baf26b08dcd3e1bb1896a04112f9
SiddiquiMonis401 <monisms15@gmail.com>
2020-06-11 23:01:23 +0500

    Refs using

--- App.js
+++ App.js
@@ -1,27 +1,11 @@
-// always returns data from a function as fresh object othersie data address will be given wherever the data is supplied and changes at one instance will chnage the data at all the other instance
-
-
-Vue.component('Greeting',{
-    template: '<p>Hi, I am {{ name }} <button v-on:click="changeName">  Change Name</button> </p> ',
-    data: function (){
-        return{
-            name:"Muhammad Monis Mazhar"
-        }
+new Vue({
+    el: "#vue-app",
+    data:{
+        favouriteFood: "N/A",
     },
     methods: {
-        changeName: function() {
-            this.name = "Muhammad Anas Mazhar";
-        }
-    }
+        changeFood: function () {
+            this.favouriteFood = this.$refs.input.value;
+        } 
+    } 
 })
-
-let one = new Vue({
-    el:"#vue-app-one",
-    
-});
-
-let two = new Vue({
-    el:"#vue-app-two",
-    
-})
-
