a13070452ff17420ed445637c9d3913792ce3c08
SiddiquiMonis401 <monisms15@gmail.com>
2020-06-11 18:56:47 +0500

    PunchBag Game covering all the concepts till now

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 new Vue({
   el: '#app',
   data: {
-      isError: false,
-      isSuccess: false,
+    isPunched: false,
+    punchedCount: 0,
+    punchedImage:false,
   },
   methods: {
+    setAndCountPunch: function () {
+        if(this.punchedImage){
+            return;
+        }
+        if(!this.isPunched)
+        {
+            this.isPunched = true;
+        }
+        this.punchedCount ++;
+        this.punchedImage = true;
+        setTimeout(()=>{
+            console.log("executed")
+            this.punchedImage = false;
 
+        },500)
+
+    },
+
+    resetCount: function() {
+        this.punchedCount = 0;
+        this.punchedImage = false;
+        this.isPunched = false; 
+    }
   },
   computed:{
 
