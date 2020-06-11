5243f4ab2f46f1714db494da0b208fa421477816
SiddiquiMonis401 <monisms15@gmail.com>
2020-06-11 19:52:04 +0500

    A quick overview of VUE COMPONENTS :)

--- App.js
+++ App.js
@@ -1,29 +1,27 @@
-let one = new Vue({
-    el:"#vue-app-one",
-    data: {
-        title: "View App One",
+// always returns data from a function as fresh object othersie data address will be given wherever the data is supplied and changes at one instance will chnage the data at all the other instance
+
+
+Vue.component('Greeting',{
+    template: '<p>Hi, I am {{ name }} <button v-on:click="changeName">  Change Name</button> </p> ',
+    data: function (){
+        return{
+            name:"Muhammad Monis Mazhar"
+        }
     },
-    computed: {
-        greet: function () {
-            return ("Hello from View app one");
+    methods: {
+        changeName: function() {
+            this.name = "Muhammad Anas Mazhar";
         }
     }
+})
+
+let one = new Vue({
+    el:"#vue-app-one",
+    
 });
 
 let two = new Vue({
     el:"#vue-app-two",
-    data: {
-        title: "View App Two",
-    },
-    methods: {
-       changeAppOneTitle: function (title) {
-        one.title = title;
-       }
-    },
-    computed: {
-        greet: function () {
-            return ("Hello from View app two");
-        }
-    }
+    
 })
 
